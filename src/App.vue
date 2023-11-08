618006bda5ad913e359e315990487402fe4f1a2f
Yuri de Snaijer <yuri.desnaijer@incentro.com>
2023-11-08 16:12:45 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card border variant="outlined">hoi</v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
